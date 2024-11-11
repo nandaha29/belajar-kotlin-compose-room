93a88885d75329a356f699d4f32e0587558d7cfb
nandaha29 <nanda.hafiza29@gmail.com>
2024-11-11 15:42:05 +0700

    done add comment of room

---
+++ app/src/main/java/com/example/inventory/data/ItemsRepository.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.inventory.data
+
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Repository that provides insert, update, delete, and retrieve of [Item] from a given data source.
+ */
+interface ItemsRepository {
+    /**
+     * Retrieve all the items from the the given data source.
+     */
+    fun getAllItemsStream(): Flow<List<Item>>
+
+    /**
+     * Retrieve an item from the given data source that matches with the [id].
+     */
+    fun getItemStream(id: Int): Flow<Item?>
+
+    /**
+     * Insert item in the data source
+     */
+    suspend fun insertItem(item: Item)
+
+    /**
+     * Delete item from the data source
+     */
+    suspend fun deleteItem(item: Item)
+
+    /**
+     * Update item in the data source
+     */
+    suspend fun updateItem(item: Item)
+}
+
+/*
+File ItenRepository -> file interface untuk operasi data pada Item
+
+// Operasi Read
+getAllItemsStream(): Flow<List<Item>>  // Mengambil semua item
+getItemStream(id: Int): Flow<Item?>    // Mengambil item berdasarkan ID
+
+// Operasi Create
+insertItem(item: Item)                 // Menambah item baru
+
+// Operasi Delete
+deleteItem(item: Item)                 // Menghapus item
+
+// Operasi Update
+updateItem(item: Item)                 // Mengupdate item
+
+- berfungsi juga untuk abtraksi layer antara source data dan ui
+- berfungsi juga untuk abtraksi layer antara source data dan ui
+ */
